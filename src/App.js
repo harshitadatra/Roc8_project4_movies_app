5957bc4c38e21d9dbe7c15397b75c70bda951aa4
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2022-12-12 19:09:48 +0530

    feat: added routing

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import { Banner } from './components/Banner'
 import { Movies } from './components/Movies'
 import { Navbar } from './components/Navbar'
 import { Pagination } from './components/Pagination'
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { Favourites } from './components/Favourites'
 
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-      <Banner/>
-      <Movies/>
-      <Pagination/>
-    </div>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <Movies />
+              <Pagination />
+            </>
+          }
+        />
+        <Route path="/favourite" element={<Favourites/>}/>
+         
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
