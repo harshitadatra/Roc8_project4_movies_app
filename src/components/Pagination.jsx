f3fce8532575ec7445b40982ad2ef51b6a0fbceb
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2022-12-17 19:16:20 +0530

    feat:pagination added

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-export const Pagination = () => {
+
+export const Pagination = ({pageProp,goAhead,goBehind}) => {
+  
   return (
     <>
       <div className="w-full flex justify-center mb-8">
-        <button className="p-2 border-2 border-r-0  rounded-l-xl border-indigo-500 text-indigo-500">
+        <button onClick={goBehind} className="p-2 border-2 border-r-0  rounded-l-xl border-indigo-500 text-indigo-500">
           Previous
         </button>
         <button className="p-2 border-2 bg-g-300 border-indigo-500 text-indigo-500">
-          1
+          {pageProp}
         </button>
-        <button className="p-2 border-2 border-l-0  rounded-r-xl border-indigo-500 text-indigo-500">
+        <button onClick={goAhead} className="p-2 border-2 border-l-0  rounded-r-xl border-indigo-500 text-indigo-500">
           Next
         </button>
       </div>
