d0fb49f77e3570b9205e6c59668bad47ac62d79b
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2022-12-25 22:22:26 +0530

    fix: minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
     <>
       <div className="border pl-12 flex space-x-8 items-center py-4">
         <img alt="logo" className="w-[50px] md:w-[60px]" src={logo}></img>
-        <Link to="/" className="text-blue-400 font-bold text:xl md:text-3xl"> Movies</Link>
+        <Link to="/" className= {"text-blue-400 font-bold text:xl md:text-3xl"}> Movies</Link>
         <Link to="/favourite" className="text-blue-400 font-bold text:xl md:text-3xl">Favourites</Link>
       </div>
     </>
